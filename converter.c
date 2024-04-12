bfefd2bdedfa0b620822eeb17bbb88801da102d2
tadLab <161091442+tadLab@users.noreply.github.com>
2024-04-12 21:28:27 +0200

    converter 1.0 + doxygen

--- converter.c
+++ converter.c
@@ -1,14 +1,42 @@
 #include <stdio.h>
 
-int Fahrenheit(int celsius){
+/**
+ * @brief Convert Celsius to Fahrenheit.
+ * 
+ * This function takes a temperature value in Celsius and
+ * converts it to Fahrenheit using the conversion formula.
+ * 
+ * @param celsius Temperature value in Celsius.
+ * @return The equivalent temperature in Fahrenheit.
+ */
+int Fahrenheit(int celsius) {
     return (celsius * 9 / 5) + 32;
 }
 
-int Celsius(int fahrenheit){
+/**
+ * @brief Convert Fahrenheit to Celsius.
+ * 
+ * This function takes a temperature value in Fahrenheit and
+ * converts it to Celsius using the conversion formula.
+ * 
+ * @param fahrenheit Temperature value in Fahrenheit.
+ * @return The equivalent temperature in Celsius.
+ */
+int Celsius(int fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
 }
 
-
+/**
+ * @brief Main function to run the temperature conversion program.
+ * 
+ * This function provides an interactive command-line interface
+ * for users to select the conversion mode (Celsius to Fahrenheit
+ * or Fahrenheit to Celsius) and input the temperature to convert.
+ * 
+ * @param argc Number of command-line arguments.
+ * @param argv Array of command-line arguments.
+ * @return 0 on successful execution.
+ */
 int main(int argc, char const *argv[]) {
     char mode;
     int temperature;
