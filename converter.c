4d707844d2f1c8a627bd8025c009ce257b0b2eb9
tadLab <161091442+tadLab@users.noreply.github.com>
2024-04-12 21:21:28 +0200

    converter 0.2

--- converter.c
+++ converter.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-int Fahrenheit(int temperature){}
+int Fahrenheit(int celsius){
+    return (celsius * 9 / 5) + 32;
+}
 
-int Celsius(int temperature){
-
+int Celsius(int fahrenheit){
+    return (fahrenheit - 32) * 5 / 9;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    char mode[1];
+    char mode;
     int temperature;
 
     printf("Please select what do you want to convert:\n");
     printf("C - Celsius to Fahrenheit, F - Fahrenheit to Celsius\n");
 
-    scanf("%c\n", &mode);
+    scanf("%c", &mode);
 
-    printf("How much %c do you want to convert?\n");
+    printf("How much %c do you want to convert?\n", mode);
     scanf("%d", &temperature);
 
     switch (mode)
     {
-    case 'C':
-        printf("%d", Celsius(temperature));
-        break;
     case 'F':
-        printf("%d", Fahrenheit(temperature));
+        printf("%d Fahrenheit is %d Celsius", temperature, Celsius(temperature));
+        break;
+    case 'C':
+        printf("%d Celsius is %d Fahrenheit", temperature, Fahrenheit(temperature));
         break;
     default:
         printf("There was an error, try again");
